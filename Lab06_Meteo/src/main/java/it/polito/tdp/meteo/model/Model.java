6c309298492ab909ca3f1718f752ae4e8f019234
VersaldoP <79835869+VersaldoP@users.noreply.github.com>
2021-04-28 16:39:42 +0200

    soluzione definitiva presa con spunto dalla soluzione proposta

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -16,6 +16,7 @@ public class Model {
 	MeteoDAO md = new MeteoDAO();
 	private String result;
 	private int min= MAX_VALUE;
+	private List<Citta> citta;
 
 	public Model() {
 		
@@ -33,14 +34,14 @@ public class Model {
 	
 	// of course you can change the String output with what you think works best
 	public String trovaSequenza(int mese) {
-//		setCitta(mese);
+
 		List<Citta> parziale = new ArrayList<>();
-		List<Citta> citta= setCitta(mese);
+		 citta= setCitta(mese);
 		int livello = 0;
 		
 		int costo = 0;
-//		costoRicorsivo(parziale,citta,costo,livello);
-		ricercaCosto(parziale,citta,costo,livello);
+
+		ricercaCosto(parziale,livello);
 		System.out.println(min);
 		return result;
 	}
@@ -54,209 +55,89 @@ public class Model {
 	}
 	
 	
-	public void ricercaCosto(List<Citta>parziale,List<Citta> citta,int costo,int livello) {
+	public void ricercaCosto(List<Citta>parziale,int livello) {
 		//Codizone Terminale
 		if(parziale.size()==NUMERO_GIORNI_TOTALI) {
+			int costo = calcolaCosto(parziale);
 			if(costo<min) {
 				min=costo;
+				System.out.println(min);
 			List<Citta> finale=new ArrayList<>(parziale);
 			this.result=finale.toString();
 			}
-			return;
+			
 		}
 		else {
 			for(Citta c: citta) {
 				//Genero la soluzione Parziale
-				if(c.getCounter()<6) {
-					if(c.getCounter()<3) {
+				if(valido(c,parziale)) {
 					parziale.add(c);
-					parziale.add(c);
-					parziale.add(c);
-					int counter = c.getCounter();
-					c.setCounter(counter+3);
-					int newCosto= costo
-									+c.getRilevamento(livello).getUmidita()
-									+c.getRilevamento(livello+1).getUmidita()
-								    +c.getRilevamento(livello+2).getUmidita();
+					ricercaCosto(parziale,livello++);
+					parziale.remove(parziale.size()-1);
 					
-//					System.out.println(newCosto);
-//					System.out.println(parziale);
-					if(parziale.size()==3) {
-//	
-//						newCosto= newCosto+COST;
-					}
-					else if(c!=parziale.get(livello-1))
-						newCosto= newCosto+COST;
-					System.out.println(parziale.size());
-					System.out.println(livello);
-					System.out.println(parziale);
-					System.out.println(costo+"---> "+newCosto);
-					ricercaCosto(parziale,citta,newCosto,livello+3);
-					//backtracking
-					c.setCounter(counter);
-					parziale.remove(c);
-					parziale.remove(c);
-					parziale.remove(c);
-				}
-//					else {
-//						 if(c!=parziale.get(livello-1)&&c.getCounter()==3) {
-//							 	parziale.add(c);
-//								parziale.add(c);
-//								parziale.add(c);
-//								int counter = c.getCounter();
-//								c.setCounter(counter+3);
-//								int newCosto= costo
-//												+c.getRilevamento(livello).getUmidita()
-//												+c.getRilevamento(livello+1).getUmidita()
-//											    +c.getRilevamento(livello+2).getUmidita();
-//								
-//
-//									newCosto= newCosto+COST;
-//								System.out.println(parziale.size());
-//								System.out.println(livello);
-//								System.out.println(parziale);
-//								ricercaCosto(parziale,citta,newCosto,livello+3);
-//								//backtracking
-//								c.setCounter(counter);
-//								parziale.remove(c);
-//								parziale.remove(c);
-//								parziale.remove(c);
-//						}
-						 else {
-						parziale.add(c);
-						int counter = c.getCounter();
-						c.setCounter(counter+1);
-						int newCosto= costo+c.getRilevamento(livello).getUmidita();
-										
-						System.out.println(parziale.size());
-						System.out.println(livello);
-						System.out.println(parziale);
-//						if(c!=parziale.get(livello))
-//							newCosto= newCosto+COST;
-						ricercaCosto(parziale,citta,newCosto,livello+1);
-						//backtracking
-						c.setCounter(counter);
-						parziale.remove(c);
-						 }
 					
-					}
 				}
+			}
 			}
 			
 		}
 	
 	
-//	public void costoRicorsivo(List<Citta> parziale,List<Citta>citta,int costo, int livello) {
-////		System.out.println(livello);
-////		System.out.println(parziale);
-////		System.out.println(citta);
-//		if(livello>NUMERO_GIORNI_TOTALI) {
-//			System.out.println("check");
-//			return;
-//		}
-//		if(livello==NUMERO_GIORNI_TOTALI&&parziale.containsAll(citta)) {
-//			int min=MAX_VALUE;
-//			
-//			if(costo<min) {
-////				List<Citta> finale = new ArrayList<>(parziale);
-//				this.result=parziale.toString();
-//			}
-//			return;
-//			
-//		
-//		}
-//		else{
-//			for(Citta c:citta) {
-//				if(valido(parziale,c,livello)) {
-//					parziale.add(c);
-////					System.out.println(parziale);
-////					System.out.println(c);
-//					c.increaseCounter();
-//					int newcosto= costo + c.getRilevamento(livello).getUmidita();
-//					if(parziale.size()>1) {
-//					if(!c.equals(parziale.get(livello-1))){
-//						newcosto = newcosto + 100;
-//					}
-//					}
-//					costoRicorsivo(parziale,citta,newcosto,livello++);
-//					parziale.remove(c);
-//					c.setCounter(c.getCounter()-1);
-//				}
-////					if(parziale.size()==0) {
-////						parziale.add(c.toString());
-////						c.increaseCounter();
-////					}
-////					else {
-////						if(parziale.size()==1&&c.equals(parziale.get(parziale.size()-1))) {
-////							parziale.add(c.toString());
-////							c.increaseCounter();
-////						}
-////						else {
-////							if((c.equals(parziale.get(parziale.size()-2)))&&(c.equals(parziale.get(parziale.size()-1)))) {
-////								parziale.add(c.toString());
-////								c.increaseCounter();
-////							}
-////						}
-////					}
-////				}
-//			}
-//		}
-//	}
+private int calcolaCosto(List<Citta> parziale) {
+		int costo= 0;
+		
+		for(int giorno=1;giorno<=NUMERO_GIORNI_TOTALI;giorno++) {
+			Citta c= parziale.get(giorno-1);
+			int rilevamento = c.getRilevamento(giorno-1).getUmidita();
+			costo+=rilevamento;
+		}
+		for(int giorno=2;giorno<=NUMERO_GIORNI_TOTALI;giorno++) {
+			if(!parziale.get(giorno-1).equals(parziale.get(giorno-2)))
+			costo+=COST;
+		}
+		
+		return costo;
+
+	}
+
+
 	
-	
-	
+	private boolean valido(Citta c, List<Citta> parziale) {
+		//verica i giorni massimi 
+		//contiamo qualte volte la citta c è comparsa nella soluzione parziale
+		int conta =0;
+		for(Citta precedente:parziale) {
+			if(precedente.equals(c))
+				conta++;
+			
+				
+		}
+		if(conta>=NUMERO_GIORNI_CITTA_MAX)
+			return false;
+		if(parziale.size()==0)
+			return true;
+		if(parziale.size()==1||parziale.size()==2) {
+			//vuol dire che siamo al secondo o al terzo giorno e quindi non possiamo cambiare citta
+			return c.equals(parziale.get(parziale.size()-1));
+		}
+		if(c.equals(parziale.get(parziale.size()-1)))
+			return true;
+		if(parziale.get(parziale.size()-1).equals(parziale.get(parziale.size()-2))&&
+				parziale.get(parziale.size()-2).equals(parziale.get(parziale.size()-3)))
+			return true;
+		
+		return false;
+	}
+
 	public List<Citta> setCitta(int mese) {
 		List<Citta> citta= new ArrayList<>();
-//		Citta Torino= new Citta("Torino",md.getAllRilevamentiLocalitaMese(mese,"Torino"));
-//		Citta Milano= new Citta("Torino",md.getAllRilevamentiLocalitaMese(mese,"Milano"));
-//		Citta Genova= new Citta("Torino",md.getAllRilevamentiLocalitaMese(mese,"Genova"));
+
 		citta.add(new Citta("Torino",md.getAllRilevamentiLocalitaMese(mese,"Torino")));
 		citta.add(new Citta("Milano",md.getAllRilevamentiLocalitaMese(mese,"Milano")));
 		citta.add(new Citta("Genova",md.getAllRilevamentiLocalitaMese(mese,"Genova")));
 		return citta;
 	}
 	
-//	public boolean valido(List<Citta> parziale,Citta c,int livello) {
-//		System.out.println(parziale.size());
-//		System.out.println(c);
-//		System.out.println(parziale);
-//		
-//		if((c.getCounter()<=6)&&((parziale.size()==0)
-//				||(parziale.size()==1&&c.equals(parziale.get(parziale.size()-1)))
-//				||((c.equals(parziale.get(parziale.size()-2)))&&(c.equals(parziale.get(parziale.size()-1)))))) {
-//				return true;
-//			}
-//		else {
-//			return false;
-//		}
-//		if(livello==0) {
-//		return true;
-//		}
-//			else {
-//				System.out.println(parziale);
-//				System.out.println(c);
-//				if(c.equals(parziale.get(livello- 1)))
-//						System.out.println("check");
-//
-//				if (c.getCounter() <= 6) {
-//					System.out.println(c.getCounter());
-//					
-//
-//					if (livello== 1 && c.equals(parziale.get(livello- 1))) {
-//						System.out.println(parziale.get(0));
-//						return true;
-//
-//					} else {
-//						if ((c.equals(parziale.get(livello - 2)))
-//								&& (c.equals(parziale.get(livello- 1)))) {
-//							return true;
-//
-//						}
-//
-//					}
-//				}
-//			}
-//			return false;
-//		}
+
 	}
 	
